37c9f4550b266490ce2d1e0881c72df7493a007e
Николай Эпаев <dvd11122@mail.ru>
2020-10-07 22:04:20 +0300

    Variables image, texture, sprite and window become global. Also added function Show to show equivalent elements from massive to window

--- TZ/main.cpp
+++ TZ/main.cpp
@@ -1,27 +1,80 @@
 #include <SFML/Graphics.hpp>
+#include "Classes.h"
+#include "Functions.h"
 
 using namespace sf;
 
+RenderWindow window(VideoMode(800, 800), "SFML Works!");
+Image image;
+Texture texture;
+Sprite sprite;
+Map element;
+
+void show();
+
 int main()
 {
-	// Объект, который, собственно, является главным окном приложения
-	RenderWindow window(VideoMode(200, 200), "SFML Works!");
-
-	// Главный цикл приложения. Выполняется, пока открыто окно
 	while (window.isOpen())
 	{
-		// Обрабатываем очередь событий в цикле
 		Event event;
 		while (window.pollEvent(event))
 		{
-			// Пользователь нажал на «крестик» и хочет закрыть окно?
 			if (event.type == Event::Closed)
-				// Тогда закрываем его
 				window.close();
 		}
-		// Отрисовка окна	
+		window.clear();
+		show();
 		window.display();
 	}
 
 	return 0;
+}
+
+void show()
+{
+	for (int i = 0; i < 6; ++i)
+	{
+		for (int j = 0; j < 5; ++j)
+		{
+			switch (element.get_elem(i, j))
+			{
+			case -1:
+			{
+				image.loadFromFile("Images/Blocked_elem.png");
+				break;
+			}
+			case 0:
+			{
+				image.loadFromFile("Images/Free_elem.png");
+				break;
+			}
+			case 1:
+			{
+				image.loadFromFile("Images/Red_elem.png");
+				break;
+			}
+			case 2:
+			{
+				image.loadFromFile("Images/Blue_elem.png");
+				break;
+			}
+			case 3:
+			{
+				image.loadFromFile("Images/Green_elem.png");
+				break;
+			}
+			default:
+				break;
+			}
+			texture.loadFromImage(image);
+			sprite.setTexture(texture);
+			sprite.setPosition(element.get_x_position(), element.get_y_position());
+			window.draw(sprite);
+			element.set_x_position(element.get_x_position() + 100);
+		}
+		element.set_x_position(10);
+		element.set_y_position(element.get_y_position() + 100);
+		if(i == 5)
+			element.set_y_position(10);
+	}
 }
