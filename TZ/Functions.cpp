3bb58717250110e0eca3752c5aef5bf71a3f05e6
Николай Эпаев <dvd11122@mail.ru>
2020-10-08 05:03:41 +0300

    fixed bug of finishing game

--- TZ/Functions.cpp
+++ TZ/Functions.cpp
@@ -8,12 +8,12 @@ int win_check(Map* elem)
 	int result = 1;
 	int col = 0;
 
-	for (col; col < 5; col += 2)
+	for (col = 0; col < 5; col += 2)
 	{
 		result = 1;
 		for (int j = 1; j < 6; ++j)
 		{
-			if (elem->get_elem(col, 0) != elem->get_elem(j, col))
+			if (elem->get_elem(0, col) != elem->get_elem(j, col))
 				result = 0;
 		}
 	}
